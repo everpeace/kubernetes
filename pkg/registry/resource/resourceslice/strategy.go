f9785b11d9edc40654fabd00725efb0b7e886a17
Shingo Omura <everpeace@gmail.com>
2026-03-16 19:05:04 +0900

    KEP-5491: strategy(dropDisableFields) for list fields in DeviceAttribute

--- pkg/registry/resource/resourceslice/strategy.go
+++ pkg/registry/resource/resourceslice/strategy.go
@@ -196,6 +196,7 @@ func dropDisabledFields(newSlice, oldSlice *resource.ResourceSlice) {
 	dropDisabledDRAPartitionableDevicesFields(newSlice, oldSlice)
 	dropDisabledDRADeviceBindingConditionsFields(newSlice, oldSlice)
 	dropDisabledDRAConsumableCapacityFields(newSlice, oldSlice)
+	dropDisableDRAListTypeAttributesFields(newSlice, oldSlice)
 }
 
 func dropDisabledDRADeviceTaintsFields(newSlice, oldSlice *resource.ResourceSlice) {
@@ -323,4 +324,48 @@ func dropDisabledDRAConsumableCapacityFields(newSlice, oldSlice *resource.Resour
 			}
 		}
 	}
+}
+
+func dropDisableDRAListTypeAttributesFields(newSlice, oldSlice *resource.ResourceSlice) {
+	if utilfeature.DefaultFeatureGate.Enabled(features.DRAListTypeAttributes) ||
+		draListTypeAttributesFeatureInUse(oldSlice) {
+		return
+	}
+
+	for i := range newSlice.Spec.Devices {
+		for k, deviceAttribute := range newSlice.Spec.Devices[i].Attributes {
+			if deviceAttribute.BoolValues != nil {
+				deviceAttribute.BoolValues = nil
+				newSlice.Spec.Devices[i].Attributes[k] = deviceAttribute
+			}
+			if deviceAttribute.IntValues != nil {
+				deviceAttribute.IntValues = nil
+				newSlice.Spec.Devices[i].Attributes[k] = deviceAttribute
+			}
+			if deviceAttribute.StringValues != nil {
+				deviceAttribute.StringValues = nil
+				newSlice.Spec.Devices[i].Attributes[k] = deviceAttribute
+			}
+			if deviceAttribute.VersionValues != nil {
+				deviceAttribute.VersionValues = nil
+				newSlice.Spec.Devices[i].Attributes[k] = deviceAttribute
+			}
+		}
+	}
+}
+
+func draListTypeAttributesFeatureInUse(slice *resource.ResourceSlice) bool {
+	if slice == nil {
+		return false
+	}
+
+	for _, device := range slice.Spec.Devices {
+		for _, deviceAttribute := range device.Attributes {
+			if deviceAttribute.BoolValues != nil || deviceAttribute.IntValues != nil || deviceAttribute.StringValues != nil || deviceAttribute.VersionValues != nil {
+				return true
+			}
+		}
+	}
+
+	return false
 }
--- pkg/registry/resource/resourceslice/strategy_test.go
+++ pkg/registry/resource/resourceslice/strategy_test.go
@@ -187,6 +187,16 @@ var sliceWithConsumableCapacity = func() *resource.ResourceSlice {
 	return obj
 }()
 
+var sliceWithListTypeAttributes = func() *resource.ResourceSlice {
+	obj := slice.DeepCopy()
+	obj.Spec.Devices[0].Attributes = map[resource.QualifiedName]resource.DeviceAttribute{
+		resource.QualifiedName("list_attribute"): {
+			StringValues: []string{"value1", "value2"},
+		},
+	}
+	return obj
+}()
+
 func TestResourceSliceStrategy(t *testing.T) {
 	if Strategy.NamespaceScoped() {
 		t.Errorf("ResourceSlice must not be namespace scoped")
@@ -205,6 +215,7 @@ func TestResourceSliceStrategyCreate(t *testing.T) {
 		bindingConditions       bool
 		deviceStatus            bool
 		consumableCapacity      bool
+		listTypeAttributes      bool
 		expectedValidationError bool
 		expectObj               *resource.ResourceSlice
 	}{
@@ -351,6 +362,20 @@ func TestResourceSliceStrategyCreate(t *testing.T) {
 				return obj
 			}(),
 		},
+		"keep-fields-list-type-attributes": {
+			obj:                sliceWithListTypeAttributes,
+			listTypeAttributes: true,
+			expectObj: func() *resource.ResourceSlice {
+				obj := sliceWithListTypeAttributes.DeepCopy()
+				obj.Generation = 1
+				return obj
+			}(),
+		},
+		"drop-fields-list-type-attributes": {
+			obj:                     sliceWithListTypeAttributes,
+			listTypeAttributes:      false,
+			expectedValidationError: true,
+		},
 	}
 
 	for name, tc := range testCases {
@@ -361,6 +386,7 @@ func TestResourceSliceStrategyCreate(t *testing.T) {
 				features.DRADeviceBindingConditions:   tc.bindingConditions,
 				features.DRAResourceClaimDeviceStatus: tc.deviceStatus,
 				features.DRAConsumableCapacity:        tc.consumableCapacity,
+				features.DRAListTypeAttributes:        tc.listTypeAttributes,
 			})
 
 			obj := tc.obj.DeepCopy()
@@ -392,6 +418,7 @@ func TestResourceSliceStrategyUpdate(t *testing.T) {
 		deviceStatus          bool
 		bindingConditions     bool
 		consumableCapacity    bool
+		listTypeAttributes    bool
 		expectValidationError bool
 		expectObj             *resource.ResourceSlice
 	}{
@@ -747,6 +774,44 @@ func TestResourceSliceStrategyUpdate(t *testing.T) {
 				obj := sliceWithCapacity.DeepCopy()
 				obj.ResourceVersion = "4"
 				obj.Generation = 1
+				return obj
+			}(),
+		},
+		"drop-list-type-attributes": {
+			oldObj: slice.DeepCopy(),
+			newObj: func() *resource.ResourceSlice {
+				obj := sliceWithListTypeAttributes.DeepCopy()
+				obj.ResourceVersion = "4"
+				return obj
+			}(),
+			listTypeAttributes:    false,
+			expectValidationError: true,
+		},
+		"keep-list-type-attributes": {
+			oldObj: sliceWithListTypeAttributes,
+			newObj: func() *resource.ResourceSlice {
+				obj := sliceWithListTypeAttributes.DeepCopy()
+				obj.ResourceVersion = "4"
+				return obj
+			}(),
+			listTypeAttributes: true,
+			expectObj: func() *resource.ResourceSlice {
+				obj := sliceWithListTypeAttributes.DeepCopy()
+				obj.ResourceVersion = "4"
+				return obj
+			}(),
+		},
+		"keep-existing-list-type-attributes-without-featuregate-enabled": {
+			oldObj: sliceWithListTypeAttributes,
+			newObj: func() *resource.ResourceSlice {
+				obj := sliceWithListTypeAttributes.DeepCopy()
+				obj.ResourceVersion = "4"
+				return obj
+			}(),
+			listTypeAttributes: false,
+			expectObj: func() *resource.ResourceSlice {
+				obj := sliceWithListTypeAttributes.DeepCopy()
+				obj.ResourceVersion = "4"
 				return obj
 			}(),
 		},
